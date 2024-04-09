578f0e1872d30fcb7b77c81e7424bda2db1bba83
doug-cresswell <104023005+doug-cresswell@users.noreply.github.com>
2024-04-09 23:23:08 +0100

    Fix docstring added to wrong src dir

--- loan_gauge/__init__.py
+++ loan_gauge/__init__.py
@@ -1,1 +1,11 @@
-"""The loan_gauge package provides tools and utilities for analyzing and visualising loan and payment schedules."""
+"""
+The loan_gauge package provides tools and utilities for analyzing and visualizing loan and
+mortgage payment schedules.
+
+Modules:
+    mortgage: Contains functions for generating mortgage payment schedules.
+    main: The main application for running the Dash web app to visualize mortgage data.
+
+This package is designed to assist users in understanding the financial implications of loans
+and mortgages over time, with a focus on principal and interest payments.
+"""

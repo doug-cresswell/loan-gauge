8823157699a94456aa1155f61c6de8e7190575a1
doug-cresswell <104023005+doug-cresswell@users.noreply.github.com>
2024-04-10 08:57:41 +0100

    Update main.py

--- loan_gauge/main.py
+++ loan_gauge/main.py
@@ -10,51 +10,83 @@ towards reducing the principal over the term of the mortgage.
 """
 
 import dash
+import plotly.express as px
 import plotly.graph_objs as go
+from dash import Input, Output, callback, dash_table, dcc, html
 from mortgage import generate_mortgage_schedule
 
 # Create dataset of mortgage monthly payments
-df_payments = generate_mortgage_schedule(
+df = generate_mortgage_schedule(
     principal=300000, annual_interest_rate=4.5 / 100, years=25
 )
 
+# Initialize the app
 app = dash.Dash(__name__)
 
-app.layout = dash.html.Div(
+
+# App layout
+app.layout = html.Div(
     children=[
-        dash.html.H1(children="Mortgage Payments Overview"),
-        dash.dcc.Graph(
-            id="mortgage-stacked-area",
-            figure={
-                "data": [
-                    go.Scatter(
-                        x=df_payments["Month"],
-                        y=df_payments["Principal Paid"],
-                        stackgroup="one",
-                        name="Principal Paid",
-                    ),
-                    go.Scatter(
-                        x=df_payments["Month"],
-                        y=df_payments["Interest Paid"],
-                        stackgroup="one",
-                        name="Interest Paid",
-                    ),
-                ],
-                "layout": go.Layout(
-                    title="Mortgage Payments (Principal vs Interest)",
-                    xaxis={"title": "Month"},
-                    yaxis={"title": "Amount Paid"},
-                ),
-            },
+        html.H1(children="Mortgage Payments Interest vs Principal"),
+        dcc.RadioItems(
+            options=[25],
+            value=25,
+            id="years",
         ),
-        dash.html.H2(children="Payment Details"),
-        dash.dash_table.DataTable(
+        dcc.Graph(id="interest-vs-principal-graph"),
+        html.H1(children="Mortgage Payments Overview"),
+        dcc.RadioItems(
+            options=df.columns,
+            value="Remaining Balance",
+            id="controls-and-radio-item",
+        ),
+        dcc.Graph(figure={}, id="controls-and-graph"),
+        html.Hr(),
+        html.H2(children="Payment Details"),
+        dash_table.DataTable(
             id="table",
-            columns=[{"name": i, "id": i} for i in df_payments.columns],
-            data=df_payments.to_dict("records"),
+            columns=[{"name": i, "id": i} for i in df.columns],
+            data=df.to_dict("records"),
         ),
     ]
 )
+
+
+# Add controls to build the interaction
+@callback(
+    Output(component_id="controls-and-graph", component_property="figure"),
+    Input(component_id="controls-and-radio-item", component_property="value"),
+)
+def update_graph(col_chosen):
+    fig = px.area(df, x="Month", y=col_chosen)
+    return fig
+
+
+@app.callback(Output("interest-vs-principal-graph", "figure"), Input("years", "value"))
+def display_area(years):
+    figure = {
+        "data": [
+            go.Scatter(
+                x=df["Month"],
+                y=df["Principal Paid"],
+                stackgroup="one",
+                name="Principal Paid",
+            ),
+            go.Scatter(
+                x=df["Month"],
+                y=df["Interest Paid"],
+                stackgroup="one",
+                name="Interest Paid",
+            ),
+        ],
+        "layout": go.Layout(
+            title="Mortgage Payments (Principal vs Interest)",
+            xaxis={"title": "Month"},
+            yaxis={"title": "Amount Paid"},
+        ),
+    }
+    return figure
+
 
 if __name__ == "__main__":
     app.run_server(debug=True)
